aaebf5929fd54a9cb5393e009dada7d3c5082a4b
eddieli <eddieli@MIKE-LI.local>
2016-06-13 15:47:29 +0800

    add swagger description

--- server/config/swagger.js
+++ server/config/swagger.js
@@ -3,8 +3,14 @@ import swaggerJSDoc from 'swagger-jsdoc';
 const options = {
   swaggerDefinition: {
     info: {
-      title: 'sc express api', // Title (required)
+      title: 'sc express api es6', // Title (required)
       version: '1.0.0', // Version (required)
+      description : "A sample project for REST CRUD API with mongoDB \n\nlogging, profile, cache, swagger, babel, EC2016",
+      contact: {
+        name: "Eddie Li",
+        url: "https://github.com/eddielisc",
+        email: "scli1989@gmail.com"
+      },
     },
     basePath : "/v1",
     tags : [
